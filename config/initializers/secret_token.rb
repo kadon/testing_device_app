98997286f81456e95c04bb1458550f85ebefcd06
Omar <oreyes@itexico.net>
2013-07-24 10:37:00 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingDeviseApp::Application.config.secret_token = '81c564c0398d4faded64d87e9190948de0fe89fe39598a823c4c4ff83bd2db0f6a359b50ffe6fd0b1ad363dabffeafc106585d4a8d7109f513da9b35fe349b77'
